6d3cc6d3a78ed2c068688d94960da5e21f9a31cd
mduanaa <dminzg@gmail.com>
2019-12-31 20:32:29 +0800

    Add files via upload

---
+++ Web_Sever/flask_web_server.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# host = '143.89.50.18'
+host = '127.0.0.1'
+port = 8097
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host=host, port=port)
+
+
